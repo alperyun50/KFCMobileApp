fc303fcbfa7a4a87fc7eea9669f2186661119aff
Alper Yavuz ÜN <yavuzun50@hotmail.com>
2021-05-06 02:19:22 +0300

    Add project files.

---
+++ KFCMobileApp/KFCMobileApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace KFCMobileApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Clicked(object sender, EventArgs e)
+        {
+            // we are holding EntUserName text data in "username"
+            Preferences.Set("username", EntUserName.Text);
+        }
+    }
+}
